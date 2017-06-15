a0216f233d75148d64c70754bf59411abf6a656c
zrain <zrainma@sina.com>
2017-06-15 22:45:35 +0800

    fix

---
+++ example/src/app.vue
@@ -0,0 +1,45 @@
+<template lang="md">
+    <!-- <div class="markdown-body">
+        <markdown></markdown>
+        <div v-html="md"></div>
+    </div> -->
+    <div>
+      <balabala>
+        > This is test.
+      </balabala>
+      <p @click="mode = !mode">
+        {{mode}}
+        <balabala>
+          > This is inner.
+        </balabala>
+      </p>
+    </div>
+</template>
+
+<script>
+// import markdown from './markdown.vue'
+// import markdown_file from './markdown-file.md'
+import 'highlight.js/styles/github.css'
+import 'github-markdown-css'
+
+export default {
+  // components: {
+  //   markdown
+  // },
+  data: function() {
+    return {
+      // md: markdown_file,
+      mode: false
+    }
+  }
+}
+</script>
+<style>
+  .markdown-body {
+    box-sizing: border-box;
+    min-width: 200px;
+    max-width: 980px;
+    margin: 0 auto;
+    padding: 45px;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './example/index.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/dist/',
+		filename: 'build.js',
+	},
+	resolve: {
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js'
+		}
+	},
+	module: {
+		rules: [{
+			test: /\.vue$/,
+			loader: 'vue-loader',
+			options: {
+				loaders: {
+					md: path.resolve(__dirname, './index.js') + "?tag=balabala"
+				}
+			}
+		}, {
+			test: /\.js$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/
+		}, {
+			test: /\.css$/,
+			loader: 'style-loader!css-loader'
+		}, {
+			test: /\.md$/,
+			use: [{
+				loader: 'raw-loader'
+			}, {
+				loader: path.resolve(__dirname, './index.js')
+			}]
+		}, {
+			test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+			loader: 'file-loader'
+		}, {
+			test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+			loader: 'file-loader',
+			query: {
+				name: '[name].[ext]?[hash]'
+			}
+		}]
+	},
+	devServer: {
+		historyApiFallback: true,
+		noInfo: true
+	},
+	devtool: '#eval-source-map'
+};
