143b38b1fdf32e79d0fd8fb948fe90adfe743df3
zrain <zrainma@sina.com>
2017-06-16 17:05:36 +0800

    fix

--- example/src/app.vue
+++ example/src/app.vue
@@ -1,88 +1,89 @@
 <template lang="md">
-    <!-- <div class="markdown-body">
-        <markdown></markdown>
-        <div v-html="md"></div>
-    </div> -->
-    <div>
+	<div>
+		<!-- 
+		<div class="markdown-body">
+			<markdown></markdown>
+			<div v-html="md"></div>
+		</div>
+		-->
+		<!-- <Markdown>
+			# Menu <span>导航菜单</span> 
 
-      <Markdown>
-        # Menu <span>导航菜单</span> 
+			为页面和功能提供导航的菜单列表。
 
-        为页面和功能提供导航的菜单列表。
+			## 何时使用
 
-        ## 何时使用
+			导航菜单是一个网站的灵魂，用户依赖导航在各个页面中进行跳转。一般分为顶部导航和侧边导航，顶部导航提供全局性的类目和功能，侧边导航提供多级结构来收纳和排列网站架构。
 
-        导航菜单是一个网站的灵魂，用户依赖导航在各个页面中进行跳转。一般分为顶部导航和侧边导航，顶部导航提供全局性的类目和功能，侧边导航提供多级结构来收纳和排列网站架构。
+			更多布局和导航的范例可以参考：常用布局。
+		</Markdown> -->
 
-        更多布局和导航的范例可以参考：常用布局。
-      </Markdown>
+		<Markdown>
+		```html
+		<div>
+			<p>水平的顶部导航菜单。</p>
+		</div>
+		```
+		</Markdown>
 
-      <Markdown>
-      ```html
-      <div>
-        <p>水平的顶部导航菜单。</p>
-      </div>
-      ```
-      </Markdown>
+		<!-- <Markdown>
+			> This is test1.
 
-      <!-- <Markdown>
-        > This is test1.
+			# Hello
 
-        # Hello
+			## h2
 
-        ## h2
+			### h3
 
-        ### h3
+			#### h4
+			
+			##### h5
 
-        #### h4
-        
-        ##### h5
+			`<span>{{sss}}</span>`
 
-        `<span>{{sss}}</span>`
+			> This is test.
 
-        > This is test.
+			- How are you?
+			- Fine, Thank you, and you?
+			- I'm fine， too. Thank you.
+			- 🌚
 
-        - How are you?
-        - Fine, Thank you, and you?
-        - I'm fine， too. Thank you.
-        - 🌚
+			```javascript
+			import Vue from 'vue'
 
-        ```javascript
-        import Vue from 'vue'
+			Vue.config.debug = true
+			```
+			```html
+			<script>
+				module.exports = {}
+			</script>
+			<script>
+				module.exports = {}
+			</script>
+			<script>
+				module.exports = {}
+			</script>
 
-        Vue.config.debug = true
-        ```
-        ```html
-        <script>
-          module.exports = {}
-        </script>
-        <script>
-          module.exports = {}
-        </script>
-        <script>
-          module.exports = {}
-        </script>
+			<template>
+				<div></div>
+			</template>
+			```
+			sadfsfs
 
-        <template>
-          <div></div>
-        </template>
-        ```
-        sadfsfs
+			大家哦哦好啊谁都发生地方上的冯绍峰s
 
-        大家哦哦好啊谁都发生地方上的冯绍峰s
-
-        > sahhhh
-        ```html
-        <compo>{{model }}{{model }}{{model }}{{model }}{{ model }}</compo>
-        ```
-      </Markdown> -->
-      <p @click="mode = !mode">
-        {{mode}}
-        <Markdown>
-          > This is inner.
-        </Markdown>
-      </p>
-    </div>
+			> sahhhh
+			```html
+			<compo>{{model }}{{model }}{{model }}{{model }}{{ model }}</compo>
+			```
+		</Markdown> -->
+		<!-- <p @click="mode = !mode">
+			{{mode}}
+			<Markdown>
+				> This is inner.
+			</Markdown>
+		</p> -->
+	</div>
 </template>
 
 <script>
@@ -92,23 +93,23 @@ import 'highlight.js/styles/github.css'
 import 'github-markdown-css'
 
 export default {
-  // components: {
-  //   markdown
-  // },
-  data: function() {
-    return {
-      // md: markdown_file,
-      mode: false
-    }
-  }
+	// components: {
+	//   markdown
+	// },
+	data: function() {
+		return {
+			// md: markdown_file,
+			mode: false
+		}
+	}
 }
 </script>
 <style>
-  .markdown-body {
-    box-sizing: border-box;
-    min-width: 200px;
-    max-width: 980px;
-    margin: 0 auto;
-    padding: 45px;
-  }
+	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
 </style>
