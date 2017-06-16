c67f5a6a1191d5750335744ab45f6f860f5e26da
zrain <zrainma@sina.com>
2017-06-16 16:41:12 +0800

    fix

--- example/src/app.vue
+++ example/src/app.vue
@@ -6,14 +6,26 @@
     <div>
 
       <Markdown>
-        ```html
-          <div>
-            <p>水平的顶部导航菜单。</p>
-          </div>
-        ```
+        # Menu <span>导航菜单</span> 
+
+        为页面和功能提供导航的菜单列表。
+
+        ## 何时使用
+
+        导航菜单是一个网站的灵魂，用户依赖导航在各个页面中进行跳转。一般分为顶部导航和侧边导航，顶部导航提供全局性的类目和功能，侧边导航提供多级结构来收纳和排列网站架构。
+
+        更多布局和导航的范例可以参考：常用布局。
       </Markdown>
 
       <Markdown>
+      ```html
+      <div>
+        <p>水平的顶部导航菜单。</p>
+      </div>
+      ```
+      </Markdown>
+
+      <!-- <Markdown>
         > This is test1.
 
         # Hello
@@ -63,7 +75,7 @@
         ```html
         <compo>{{model }}{{model }}{{model }}{{model }}{{ model }}</compo>
         ```
-      </Markdown>
+      </Markdown> -->
       <p @click="mode = !mode">
         {{mode}}
         <Markdown>
